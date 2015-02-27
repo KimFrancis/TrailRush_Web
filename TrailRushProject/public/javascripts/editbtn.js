b6f5b1e25a7b8ef2ca7bab86add2cb486940ea7a
gabzeus555 <gabrielzeus_reyes@yahoo.com.ph>
2015-02-27 15:44:38 -0800

    --WEBSITE

--- TrailRushProject/public/javascripts/editbtn.js
+++ TrailRushProject/public/javascripts/editbtn.js
@@ -7,7 +7,7 @@ $(document).ready(function(){
    $form.submit(function(){
       $.post($(this).attr('action'), $(this).serialize(), function(response){
       },'json');
-      alert('station was saved');
+      alert('Profile Save!');
       $("input", $(this)).attr("disabled", true);
       return false;
    });
